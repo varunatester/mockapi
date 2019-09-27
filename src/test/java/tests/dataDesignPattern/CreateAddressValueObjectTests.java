0a9ef8e98ba2ce4a52e25c51c4aa486dcd377318
Varuna Srivastava <srivastavavaruna@gmail.com>
2019-09-27 15:50:12 -0400

    Varuna Refactoring the tests

--- src/test/java/tests/dataDesignPattern/CreateAddressValueObjectTests.java
+++ src/test/java/tests/dataDesignPattern/CreateAddressValueObjectTests.java
@@ -1,10 +1,8 @@
 package tests.dataDesignPattern;
-
 import entities.CreateAddressRequest;
 import org.testng.annotations.Test;
 import utils.Categories;
 import java.io.IOException;
-
 import static com.jayway.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.notNullValue;
 
